52b2e13e39ca1d39d33104f8f0fc54ce277c0297
Bryce Wood <woodbr@oregonstate.edu>
2019-07-08 23:15:08 -0400

    Updated/Renamed Model

---
+++ Stranded/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+
+    public static int loadingScreen = 1;
+    [SerializeField]
+    private readonly float delayBeforeRestartingLevel = 5f;
+
+    private float timeElapsed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeRestartingLevel)
+        {
+            SceneManager.LoadScene(loadingScreen);
+        }
+    }
+}
