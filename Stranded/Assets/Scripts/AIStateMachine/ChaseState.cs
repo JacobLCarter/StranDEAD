65750aff040b6b0702d2b91d4ad5ff19cb00eb8c
Jacob Carter <32877068+JacobLCarter@users.noreply.github.com>
2019-07-18 23:44:57 -0400

    Clean up AI scripts
    - Improve animations
    - Change AI behavior to state machine
    - Add attack state/animation

---
+++ Stranded/Assets/Scripts/AIStateMachine/ChaseState.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using GeneralStateMachine;
+
+public class ChaseState : State<Enemy>
+{
+    private static ChaseState _instance;
+
+    private ChaseState()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+
+        _instance = this;
+    }
+
+    public static ChaseState Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                new ChaseState();
+            }
+
+            return _instance;
+        }
+    }
+    
+    public override void EnterState(Enemy enemy)
+    {
+        enemy.navmesh.speed = 1.5f;
+        enemy.currentStalk = enemy.stalkTime;
+    }
+
+    public override void ExitState(Enemy enemy)
+    {
+
+    }
+
+    public override void UpdateState(Enemy enemy)
+    {
+        Chase(enemy);
+        
+        enemy.isPlayerInSight();
+    }
+    
+    private void Chase(Enemy enemy)
+    {
+        if (enemy.player != null)
+        {
+            Vector3 target = enemy.player.position;
+            enemy.navmesh.SetDestination(target);
+        }
+    }
+}
---
+++ Stranded/Assets/Scripts/PlayerMovement/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class CameraFollow : MonoBehaviour
+{
+    private Transform cam;
+    private Vector3 forwardCam;
+    private Vector3 calculatedMove;
+    private float sensitivity = 3f;
+    private PlayerMovement movement;
+
+
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        //references to necessary objects
+        movement = GameObject.FindObjectOfType<PlayerMovement>();
+        cam = Camera.main.transform;
+
+        //hides the cursor when the game is being played
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //get player input on x and z axis
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //set a vector equal to the position that the camera is currently facing
+        forwardCam = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+        //calculate the player's input with the current conditions
+        calculatedMove = horizontal * cam.right + vertical * forwardCam;
+        
+        //move the player; called in the PlayerMovement script
+        movement.MovePlayer(calculatedMove, horizontal, vertical);
+
+        //get the camera's rotation for the current frame
+        float camRotation = Input.GetAxis("Mouse X");
+        Vector3 rotation = new Vector3 (0, camRotation, 0) * sensitivity;
+
+        //rotate the player; called in the PlayerMovement script
+        movement.RotatePlayer(rotation);
+    }
+}
