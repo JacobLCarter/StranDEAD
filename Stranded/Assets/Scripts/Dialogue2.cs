e0cd265ab138e31a5564b492cd366e64ad383722
Tom Pham <phamt2@oregonstate.edu>
2019-07-20 23:07:10 -0400

    Updated with dialogue

---
+++ Stranded/Assets/Scripts/Dialogue2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//REFERENCED: https://www.youtube.com/watch?v=CNNeD9oT4DY
+public class Dialogue2 : MonoBehaviour
+{
+    public GameObject secondDialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondDialogue.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Playertag")
+        {
+            secondDialogue.SetActive(true);
+            StartCoroutine(tempDialogue());
+        }
+    }
+
+    IEnumerator tempDialogue()
+    {
+        yield return new WaitForSeconds(5);
+
+        secondDialogue.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
