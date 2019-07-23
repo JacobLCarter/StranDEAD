752bb48fd6eff2f50c15b750942a9bd0a2e80c88
Bryce Wood <woodbr@oregonstate.edu>
2019-07-23 19:03:10 -0400

    Fixed menu scaling, colliders, and loading screens

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+
+    public static int deathScreen = 5;
+    [SerializeField] private readonly float delayBeforeRestartingLevel = 3f;
+
+    private float timeElapsed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeRestartingLevel)
+        {
+            //load level_1
+            SceneManager.LoadScene(3);
+        }
+    }
+}
