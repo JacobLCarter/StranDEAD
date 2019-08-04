af72aff3247f38ced748767a5a10c3f8a9a4327d
Bryce Wood <woodbr@oregonstate.edu>
2019-08-04 09:31:19 -0400

    Fixed mission text based on colliders

--- Assets/Scripts/MissionObjective.cs
+++ Assets/Scripts/MissionObjective.cs
@@ -1,29 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class MissionObjective : MonoBehaviour
 {
-    public GameObject missionObject;    
-       
+    public GameObject missionObject;
+    public Text missionCar;
+    public Text missionDam;
+    public Text missionDoor;
+    public Text missionKey;
+
     // Start is called before the first frame update
     void Start()
     {
-        missionObject.SetActive(false);
+        missionCar.enabled = false;
+        missionDam.enabled = false;
+        missionDoor.enabled = false;
+        missionKey.enabled = false;
     }
 
     void OnTriggerEnter(Collider Player)
     {
-        if (Player.gameObject.tag == "Playertag")
+        if (!missionCar.enabled && gameObject.tag == "Car")
         {
-            missionObject.SetActive(true);
+            missionCar.enabled = true;
+            missionCar.color = Color.white;
         }
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
+        else if (!missionDam.enabled && gameObject.tag == "Dam")
+        {
+            missionDam.enabled = true;
+            missionDam.color = Color.white;
+            missionCar.color = Color.red;
+        }
+        else if (!missionDoor.enabled && gameObject.tag == "Door")
+        {
+            // TODO: check if key in inventory
+            missionDoor.enabled = true;
+            missionDoor.color = Color.white;
+            missionDam.color = Color.red;
+        }
+        else if (!missionKey.enabled && gameObject.tag == "Key")
+        {
+            missionKey.enabled = true;
+            missionKey.color = Color.white;
+            missionDoor.color = Color.red;
+        }
     }
 }
