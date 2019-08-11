cef5a95ba3e017fe1392aaa7732e0393a2619799
Bryce Wood <woodbr@oregonstate.edu>
2019-08-11 19:32:39 -0400

    added staggered roof lighting effect activated with 'F' key

---
+++ Assets/Scripts/RoofLightButton.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoofLightButton : MonoBehaviour
+{
+    private GameObject[] roofLights;
+    private bool lightsOn = false;
+    private int lightIdx = 0;
+    private float timeElapsed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (roofLights == null)
+        {
+            roofLights = GameObject.FindGameObjectsWithTag("RoofLight");
+            foreach(GameObject roofLight in roofLights)
+            {
+                roofLight.SetActive(false);
+            }
+            lightIdx = roofLights.Length;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // in mode where lights need to be turned on?
+        if (lightsOn)
+        {
+            // all lights turned on?
+            if (lightIdx == 0)
+            {
+                return;
+            }
+
+            // turn on row of lights every 500 ms
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed < 0.5f)
+            {
+                // too early
+                return;
+            }
+            // reset timer
+            timeElapsed = 0;
+
+            // turn on row  (3 lights per row)
+            for (int i = 0; i < 3; i++)
+            {
+                if (lightIdx == 0)
+                {
+                    return;
+                }
+                // go in reverse order (from control room towards start of level)
+                lightIdx--;
+                roofLights[lightIdx].SetActive(true);
+            }
+        }
+        else
+        {
+            // enter lights-on mode
+            lightsOn = Input.GetKeyDown(KeyCode.F);
+        }
+    }
+}
