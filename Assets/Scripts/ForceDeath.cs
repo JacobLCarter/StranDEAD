f7fb663c11848276253908573e3b85384e37a988
Jacob Carter <32877068+JacobLCarter@users.noreply.github.com>
2019-08-12 15:14:14 -0400

    Add axe and static zombie to second level

---
+++ Assets/Scripts/ForceDeath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceDeath : MonoBehaviour
+{
+    public Transform player;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.position) < 0.5f)
+        {
+            animator.SetBool("isDead", true);
+        }
+    }
+}
