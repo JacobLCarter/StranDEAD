116c17fbcd09cef85104fd62150134fed999c087
Jacob Carter <32877068+JacobLCarter@users.noreply.github.com>
2019-08-13 16:40:36 -0400

    Add zombie idle animations, add mutant roar

---
+++ Assets/Scripts/RandomIdleAnimation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomIdleAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private float timer;
+
+    private const float maxTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= maxTime)
+        {
+            float rand = Random.Range(0f, 3f);
+            animator.SetFloat("Blend", rand);
+            timer = 0f;
+        }
+    }
+}
